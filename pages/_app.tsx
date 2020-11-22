11b2f395f7154386428dbf4c17b81abbf6adc79c
Faip Mardoni <faipmardoni@gmail.com>
2020-11-23 02:21:03 +0700

    integration redux with store

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
+import { Provider } from 'react-redux';
+import store from 'store';
 
 import 'sanitize.css';
 import 'styles/app.scss';
 
-const App: React.FC<AppProps> = ({ Component, pageProps }) => <Component {...pageProps} />;
+const App: React.FC<AppProps> = ({ Component, pageProps }) => (
+  <Provider store={store}>
+    <Component {...pageProps} />
+  </Provider>
+);
 
 export default App;
